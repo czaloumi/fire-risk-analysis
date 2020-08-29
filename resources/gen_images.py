428fd3e1da72dc3c6b28b58a2f2a409ea22a68ab
Chelsea Zaloumis <chelseazaloumis@Chelseas-MacBook-Pro.local>
2020-08-29 11:25:42 -0600

    small edits

--- resources/gen_images.py
+++ resources/gen_images.py
@@ -3,7 +3,7 @@ import numpy as np
 from numpy import random
 import tensorflow
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-from PIL import ImageFile
+from PIL import ImageFile, Image
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 from tqdm import tqdm
 
@@ -35,27 +35,27 @@ datagen = ImageDataGenerator(
 
 def gen_images(path, num, fire):
     '''
-    Reads in image, coverts to array, reshapes, applies datagen 20 times.
+    Reads in image, coverts to array, reshapes, applies datagen 4 times.
     Repeats num times.
     '''
     if fire:
-        for i in tqdm(range(28, num)):
+        for i in tqdm(range(1, num+1)):
             
-            img = load_img('{}/fire.{}.png'.format(path, i))
+            img = Image.open(f'{path}/fire.{i}.png')
             x = img_to_array(img)
             x = x.reshape((1,) + x.shape)
             i=0
-            for batch in datagen.flow(x, batch_size=1, save_to_dir='{}/'.format(path), save_prefix='fire', save_format='png'):
+            for batch in datagen.flow(x, batch_size=1, save_to_dir=f'{path}/', save_prefix='fire', save_format='png'):
                 i += 1
                 if i > 20:
                     break
     else:
-        for i in tqdm(range(28, num)):
-            img = load_img('{}/non_fire.{}.png'.format(path, i))
+        for i in tqdm(range(1, num+1)):
+            img = Image.open(f'{path}/non_fire.{i}.png')
             x = img_to_array(img)
             x = x.reshape((1,) + x.shape)
             i=0
-            for batch in datagen.flow(x, batch_size=1, save_to_dir='{}/'.format(path), save_prefix='non_fire', save_format='png'):
+            for batch in datagen.flow(x, batch_size=1, save_to_dir=f'{path}/', save_prefix='non_fire', save_format='png'):
                 i += 1
-                if i > 20:
+                if i > 4:
                     break
